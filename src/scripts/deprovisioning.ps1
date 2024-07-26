56391257d20e28bf9f9f2619e6afc40d33329845
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2024-07-26 18:06:14 +0200

    feat: removed winget specific steps

--- src/scripts/deprovisioning.ps1
+++ src/scripts/deprovisioning.ps1
@@ -1,21 +1,3 @@
-# Define an array of known problematic appx packages
-$packagesToRemove = @(
-    "Microsoft.Winget.Source*",
-    "Microsoft.Ink.Handwriting*"
-)
-
-# Remove known problematic appx packages
-Write-Output "Removing known problematic appx packages..."
-foreach ($package in $packagesToRemove) {
-    try {
-        Get-AppxPackage | Where-Object Name -Like $package | Remove-AppxPackage
-        Write-Output "Successfully removed package: $package"
-    }
-    catch {
-        Write-Output "Failed to remove package: $package. Error: $_"
-    }
-}
-
 # Ensure Guest Agent services are running
 $guestAgentServices = @("RdAgent", "WindowsAzureTelemetryService", "WindowsAzureGuestAgent")
 foreach ($service in $guestAgentServices) {
@@ -42,13 +24,14 @@ Start-Process -FilePath $sysprepPath -ArgumentList $sysprepArgs
 Write-Output "Waiting for Sysprep to complete..."
 while($true) {
     $imageState = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State | Select-Object -ExpandProperty ImageState
-    if($imageState -ne 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') {
+    if($imageState -eq 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') {
         Write-Output "Current state: $imageState"
-        Start-Sleep -Seconds 10
-    } else {
         Write-Output "Sysprep completed successfully."
         Start-Sleep -Seconds 30
         break
+    } else {
+        Write-Output "Current state: $imageState"
+        Start-Sleep -Seconds 10
     }
 }
 
