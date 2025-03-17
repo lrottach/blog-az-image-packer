3b53c06105a37665506a27b9a76d8159581aea3d
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2025-03-17 19:35:22 +0000

    feat: update workflow to dynamically query image version

--- src/outputs.tf
+++ src/outputs.tf
@@ -3,12 +3,28 @@ output "resource_group_id" {
   value       = azurerm_resource_group.main.id
 }
 
+output "resource_group_name" {
+  description = "The name of the Resource Group."
+  value       = azurerm_resource_group.main.name
+}
+
 output "gallery_id" {
   description = "The ID of the Azure Compute Gallery."
   value       = azurerm_shared_image_gallery.main.id
 }
 
+output "gallery_name" {
+  description = "The name of the Azure Compute Gallery."
+  value       = azurerm_shared_image_gallery.main.name
+}
+
 output "image_definition_id" {
   description = "The ID of the image definition."
   value       = azurerm_shared_image.main.id
+}
+
+output "image_definition_name" {
+  description = "The name of the image definition."
+  value       = azurerm_shared_image.main.name
+
 }
